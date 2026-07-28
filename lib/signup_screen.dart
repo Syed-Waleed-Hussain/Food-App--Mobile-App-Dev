7a5328fe19a1b5ac4ad2bb8a8addf2192a58170c
Syed-Waleed-Hussain <syedw4420@gmail.com>
2026-07-28 14:21:37 +0500

    initial commit

---
+++ lib/signup_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Sign_up_screen extends StatefulWidget {
+  const Sign_up_screen({super.key});
+
+  @override
+  State<Sign_up_screen> createState() => _Sign_up_screenState();
+}
+
+class _Sign_up_screenState extends State<Sign_up_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
